107c3d1f41cdb772dccd9ae0c7428d2026062a95
Diego Rodrigues <143212073+diego-pr@users.noreply.github.com>
2023-09-28 14:32:15 -0300

    Updating logos and widget banner.

---
+++ build/resources/main/admin/widgets/nynorskoversetteren/nynorskoversetteren.js
@@ -0,0 +1,62 @@
+const Thymeleaf = require('/lib/thymeleaf')
+const Portal = require('/lib/xp/portal')
+const Content = require('/lib/xp/content')
+
+const MyLicense = require('/lib/modules/license')
+const Util = require('/lib/modules/util')
+
+exports.get = function(req) {
+    const view = resolve('nynorskoversetteren.html')
+    let model = {}
+
+    // Validate the license
+    if (!MyLicense.isCurrentLicenseValid()) {
+        model = {
+            url: req.url,
+            noLicense: true,
+            licenseServiceUrl: Portal.serviceUrl({ service: 'license', type: 'absolute' }),
+            translate: {
+                noLicense: Util.localize('widgets.nynorskoversetteren.view.no_license'),
+                uploadLicense: Util.localize('widgets.nynorskoversetteren.view.upload_license'),
+                invalidLicense: Util.localize('widgets.nynorskoversetteren.view.invalid_license')
+            }
+        }
+
+        return {
+            contentType: 'text/html',
+            body: Thymeleaf.render(view, model)
+        }
+    }
+
+    const contentId = req.params.contentId
+    const siteConfig = (contentId && Content.getSiteConfig({
+        key: contentId,
+        applicationKey: app.name
+    })) || {}
+    const apiKey = siteConfig.api_key
+
+    if (!apiKey) {
+        return {
+            contentType: 'text/html',
+            body: `<widget class="error">${Util.localize('widgets.nynorskoversetteren.view.error_message_api_key')}</widget>`
+        }
+    }
+
+    model = {
+        serviceUrl: Portal.serviceUrl({ service: 'translate', type: 'absolute' }),
+        apiKey: apiKey,
+        translate: {
+            input_placeholder: Util.localize('widgets.nynorskoversetteren.view.input_placeholder'),
+            alt_header: Util.localize('widgets.nynorskoversetteren.view.alt_header'),
+            alt_translate_btn: Util.localize('widgets.nynorskoversetteren.view.alt_translate_btn'),
+            alt_copy_btn: Util.localize('widgets.nynorskoversetteren.view.alt_copy_btn'),
+            error_message: Util.localize('widgets.nynorskoversetteren.view.error_message'),
+            alt_copy_btn_success: Util.localize('widgets.nynorskoversetteren.view.alt_copy_btn_success')
+        }
+    }
+
+    return {
+        contentType: 'text/html',
+        body: Thymeleaf.render(view, model)
+    }
+}
---
+++ build/resources/main/assets/js/nynorskoversetteren.js
@@ -0,0 +1,132 @@
+var translateBtn;
+var copyBtn;
+var uploadBtn;
+
+initialize();
+
+function initialize() {
+    translateBtn = document.getElementById('nynorskoversetteren_translate_btn');
+    copyBtn = document.getElementById('nynorskoversetteren_copy_btn');
+    uploadBtn = document.getElementById('nynorskoversetteren_upload_input');
+
+    if (translateBtn) {
+        translateBtn.addEventListener('click', function () {
+            translateText(this)
+        }, false)
+    }
+
+    if (copyBtn) {
+        copyBtn.addEventListener('click', function () {
+            copyToClipboard(this)
+        }, false)
+    }
+
+    if (uploadBtn) {
+        uploadBtn.addEventListener('click', function () {
+            this.querySelector('input').click()
+        }, false)
+
+        uploadBtn.querySelector('input').addEventListener('change', function (e) {
+            var serviceUrl = this.getAttribute('data-service-url');
+            var licenseFile = e.target.files[0];
+
+            if (licenseFile) {
+                var formData = new FormData();
+                formData.append('license', licenseFile);
+
+                fetch(serviceUrl, {
+                    method: 'POST',
+                    body: formData
+                })
+                .then(response => {
+                    if (response.ok) {
+                        document.querySelector('.nynorskoversetteren-license .error-message').style.display = 'none';
+                        reloadWidget();
+                    } else {
+                        document.querySelector('.nynorskoversetteren-license .error-message').style.display = 'block';
+                    }
+                })
+                .catch(error => {
+                    document.querySelector('.nynorskoversetteren-license .error-message').style.display = 'none';
+                })
+            }
+        })
+    }
+}
+
+function reloadWidget() {
+    const widget = document.querySelector('#nynorskoversetteren');
+    const widgetUrl = widget.getAttribute('data-widget-url');
+
+    fetch(widgetUrl, {
+        method: 'GET'
+    })
+    .then(response => response.text())
+    .then(response => {
+        if (response) {
+            widget.parentElement.innerHTML = response;
+            initialize()
+        }
+    })
+}
+
+function copyToClipboard(elem) {
+    var copyText = document.getElementById('nynorskoversetteren_copy_input');
+    var tooltipCopyMessage = elem.getAttribute('data-copy-message');
+    var tooltipSuccessMessage = elem.getAttribute('data-success-message');
+
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    navigator.clipboard.writeText(copyText.value);
+
+    elem.setAttribute('data-tooltip', tooltipSuccessMessage)
+
+    elem.addEventListener('mouseleave', function () {
+        elem.setAttribute('data-tooltip', tooltipCopyMessage)
+    })
+}
+
+function translateText(elem) {
+    var text = document.getElementById('nynorskoversetteren_translate_input').value;
+    var copyText = document.getElementById('nynorskoversetteren_copy_input');
+    var errorElem = document.getElementById('nynorskoversetteren-error');
+    var serviceUrl = elem.getAttribute('data-service-url');
+    var apiKey = elem.getAttribute('data-api-key');
+    var loader = elem.querySelector('.loader');
+    var btnImage = elem.querySelector('img');
+
+    copyText.value = '';
+    errorElem.style.display = 'none';
+
+    if (serviceUrl && apiKey) {
+        btnImage.style.visibility = 'hidden';
+        loader.style.visibility = 'visible';
+        elem.disabled = true;
+
+        fetch(serviceUrl, {
+            method: 'POST',
+            body: JSON.stringify({ text: text }),
+            headers: {
+                'x-api-key': apiKey
+            }
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (typeof response.text === 'string') {
+                copyText.value = response.text;
+            } else {
+                errorElem.style.display = 'block';
+            }
+        })
+        .catch(error => {
+            errorElem.style.display = 'block';
+            console.error('Error: ', error);
+        })
+        .finally(() => {
+            btnImage.style.visibility = 'visible';
+            loader.style.visibility = 'hidden';
+            elem.disabled = false;
+        })
+    }
+}
